7c88113ad6f29454acad7e51776afae230be74fd
Luís Fillipe <lfmcpereira@sga.pucminas.br>
2022-03-11 18:41:52 -0300

    Exercicio 01

--- exemplo-git/src/exemplo/Exemplo.java
+++ exemplo-git/src/exemplo/Exemplo.java
@@ -1,8 +1,20 @@
 package exemplo;
 
-public class Exemplo {
-	public static void main(String[] args) {
-		System.out.println("Olá Git!");
+import java.util.*;
+
+class Exemplo {
+public static Scanner sc = new Scanner(System.in);
+public static void main (String args[]){
+	//Declaracao de variaveis
+	int num1, num2, soma;
+	//Leituras
+	System.out.println("Digite um número");
+	num1 = sc.nextInt();
+	System.out.println("Digite outro número");
+	num2 = sc.nextInt();
+	//Somar
+	soma = num1 + num2;
+	//Mostrar na tela
+	System.out.println("Soma:" + soma);
 	}
 }
-//git
